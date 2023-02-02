078897c72ad565ae8311095ff5725b9f61ad3318
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-02-02 08:03:39 +0500

    conf parametirlari

--- core/settings.py
+++ core/settings.py
@@ -76,32 +76,23 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DB_ENGINE   = os.getenv('DB_ENGINE'   , None)
 DB_USERNAME = os.getenv('DB_USERNAME' , None)
 DB_PASS     = os.getenv('DB_PASS'     , None)
 DB_HOST     = os.getenv('DB_HOST'     , None)
 DB_PORT     = os.getenv('DB_PORT'     , None)
 DB_NAME     = os.getenv('DB_NAME'     , None)
 
-if DB_ENGINE and DB_NAME and DB_USERNAME:
-    DATABASES = { 
-      'default': {
-        'ENGINE'  : 'django.db.backends.' + DB_ENGINE, 
-        'NAME'    : DB_NAME,
-        'USER'    : DB_USERNAME,
-        'PASSWORD': DB_PASS,
-        'HOST'    : DB_HOST,
-        'PORT'    : DB_PORT,
-        }, 
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'db.sqlite3',
-        }
-    }
 
+DATABASES = { 
+    'default': {
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME'    : DB_NAME,
+    'USER'    : DB_USERNAME,
+    'PASSWORD': DB_PASS,
+    'HOST'    : DB_HOST,
+    'PORT'    : DB_PORT,
+    }, 
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -125,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "Asia/Samarkand"
 
 TIME_ZONE = "UTC"
 
